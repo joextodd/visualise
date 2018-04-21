88da5fcd86b8f8f1ad1da81a0827c97660112856
joextodd <joextodd@gmail.com>
2018-04-21 17:09:43 +0100

    Added equaliser

--- src/visualisers/waveform/index.js
+++ src/visualisers/waveform/index.js
@@ -1,19 +1,26 @@
 const canvas = document.getElementById('visualiser')
-const scopeContext = canvas.getContext('2d')
+const ctx = canvas.getContext('2d')
 
 export default {
+
+  init: (analyser) => {
+    canvas.width = analyser.frequencyBinCount
+    canvas.height = window.innerHeight
+    canvas.style.height = '100vh';
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  },
 
   process: (analyser) => {
     const waveform = new Float32Array(analyser.frequencyBinCount)
     analyser.getFloatTimeDomainData(waveform)
 
-    scopeContext.clearRect(0, 0, canvas.width, canvas.height)
-    scopeContext.beginPath()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.beginPath()
     for (let x = 0; x < waveform.length; x++) {
       const y = (0.5 + waveform[x] / 2) * canvas.height;
-      x == 0 ? scopeContext.moveTo(x, y) : scopeContext.lineTo(x, y)
+      x == 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y)
     }
-    scopeContext.stroke()
+    ctx.stroke()
   }
 
 }
