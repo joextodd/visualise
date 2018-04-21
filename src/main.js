aeaf4c713e4c1af2fd4e5488e6a634e8a51be453
joextodd <joextodd@gmail.com>
2018-04-21 15:30:49 +0100

    Use microphone not YouTube streams

--- src/main.js
+++ src/main.js
@@ -1,100 +1,35 @@
-import { getAudioUrl, getNextVideo, getAudioStream } from  './api';
-// import { Equaliser } from './visualisers/equaliser';
-// import { CircleOfLife } from './visualisers/speaker';
 import Waveform from './visualisers/waveform';
 import './index.scss';
 
-const loadButton = document.querySelector('.load');
-const urlInput = document.querySelector('input');
-const titleText = document.querySelector('video-title');
-const nextButton = document.querySelector('.arrow-right');
-const audio = document.querySelector('audio');
-const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+const context = new (window.AudioContext || window.webkitAudioContext)()
+const canvas = document.getElementById('visualiser')
+const scopeContext = canvas.getContext('2d')
+const visualisers = new Array(Waveform)
+let vIndex = 0
 
-// const visualisers = new Array();
-const context = new (window.AudioContext || window.webkitAudioContext)();
-const analyser = context.createAnalyser();
-const source = context.createMediaElementSource(audio);
-source.connect(analyser);
-analyser.connect(context.destination);
+navigator.mediaDevices.getUserMedia({ audio: true, video: false, echoCancellation: true })
+.then(function (stream) {
+  const source = context.createMediaStreamSource(stream)
+  const processor = context.createScriptProcessor(0, 1, 1)
+  const analyser = context.createAnalyser()
 
-Waveform(analyser);
+  source.connect(processor)
+  source.connect(analyser)
+  processor.connect(context.destination)
 
-const loadAudio = (id, url, title) => {
+  canvas.width = analyser.frequencyBinCount
+  canvas.height = window.innerHeight
 
-  audio.paused ? audio : audio.pause();
-  audio.src = url;
-  audio.play();
-
-  titleText.innerHTML = title;
-
-  getNextVideo(id)
-  .then((upNext) => {
-    nextVideo = upNext;
-  });
-};
-
-const nextAudio = () => {
-  audio.paused ? audio : audio.pause();
-  if (autoplay) {
-    getAudioUrl(nextVideo)
-    .then((data) => loadAudio(nextVideo, getAudioStream(data.url), data.title));
+  processor.onaudioprocess = function(e) {
+    visualisers[vIndex].process(e.inputBuffer.getChannelData(0))
+    e.outputBuffer.getChannelData(0).forEach((v) => v = 0)
   }
-};
+})
+.catch((err) => window.alert(err))
 
-/*
- * Grab video id from query param.
- */
-const loadVideoUrl = () => {
-  const videoId = window.location.href.split('id=');
-  if (videoId.length > 1 && videoId[1].length === 11) {
-    getAudioUrl(videoId[1])
-    .then((data) => loadAudio(videoId[1], getAudioStream(data.url), data.title));
-  }
-};
-
-/*
- * Get autoplay from query param.
- */
-const loadAutoplay = () => {
-  const autoParam = window.location.href.split('autoplay=');
-  autoplay = !(autoParam.length > 1 && autoParam[1] === 'false');
-};
-
-/*
- * iOS seems to add a length of silence
- * at the end of the stream, so load next
- * track when we are passed half way.
- */
-audio.ontimeupdate = () => {
-  if (iOS && audio.currentTime > audio.duration / 2) {
-    nextAudio();
-  }
-};
-
-/*
- * On click get video audio stream.
- */
-loadButton.onclick = () => {
-  const re = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
-  const match = urlInput.value.match(re);
-
-  if (match.length > 1) {
-    getAudioUrl(match[1])
-    .then((data) => loadAudio(match[1], getAudioStream(data.url), data.title));
-  } else {
-    window.alert('Not a valid YouTube URL');
-  }
-};
-
-audio.onended = () => nextAudio()
-
-/*
- * Load next visualiser.
- */
-// nextButton.onclick = () => {
-//   clearScene();
-//   vision = (vision + 1) % visualisers.length;
-//   visualisers[vision].init();
-//   visualisers[vision].draw(scene);
-// };
+canvas.onclick = () => {
+  scopeContext.clearRect(0, 0, canvas.width, canvas.height)
+  vIndex = (vIndex + 1) % visualisers.length
+  console.log('next visualiser')
+}
--- src/visualisers/waveform/index.js
+++ src/visualisers/waveform/index.js
@@ -1,27 +1,16 @@
+const canvas = document.getElementById('visualiser')
+const scopeContext = canvas.getContext('2d')
 
-export default (analyser) => {
-  const waveform = new Float32Array(analyser.frequencyBinCount)
-  analyser.getFloatTimeDomainData(waveform)
+export default {
 
-  ;(function updateWaveform() {
-    requestAnimationFrame(updateWaveform)
-    analyser.getFloatTimeDomainData(waveform)
-  })()
-
-  const canvas = document.getElementById('visualiser')
-  canvas.width = waveform.length
-  canvas.height = window.innerHeight - 138
-  const scopeContext = canvas.getContext('2d')
-
-  ;(function drawOscilloscope() {
-    requestAnimationFrame(drawOscilloscope)
+  process: (data) => {
     scopeContext.clearRect(0, 0, canvas.width, canvas.height)
     scopeContext.beginPath()
-    for (let i = 0; i < waveform.length; i++) {
-      const x = i
-      const y = (0.5 + waveform[i] / 2) * canvas.height;
-      i == 0 ? scopeContext.moveTo(x, y) : scopeContext.lineTo(x, y)
+    for (let x = 0; x < data.length; x++) {
+      const y = (0.5 + data[x] / 2) * canvas.height;
+      x == 0 ? scopeContext.moveTo(x, y) : scopeContext.lineTo(x, y)
     }
     scopeContext.stroke()
-  })()
+  }
+
 }
