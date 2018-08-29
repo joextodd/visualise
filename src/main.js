b23cd57a09c8b3d4abf72f4a11d3481ad2a22a99
joextodd <joextodd@gmail.com>
2018-08-29 21:23:56 +0100

    Revert spectrum

--- src/main.js
+++ src/main.js
@@ -5,43 +5,33 @@ import './index.scss'
 
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
 const canvas = document.getElementById('visualiser')
-const button = document.getElementById('start')
 const visualisers = new Array(Waveform, Equaliser, Spectrum)
 isSafari ? visualisers.shift() : visualisers
-let initialised = false;
 let vIndex = 0
-let analyser;
+let analyser
 
-const start = () => {
-  const context = new (window.AudioContext || window.webkitAudioContext)()
-  const processor = context.createScriptProcessor(0, 1, 1)
-  analyser = context.createAnalyser()
+const context = new (window.AudioContext || window.webkitAudioContext)()
+const processor = context.createScriptProcessor(0, 1, 1)
+analyser = context.createAnalyser()
 
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false, echoCancellation: true })
-  .then(function (stream) {
-    const source = context.createMediaStreamSource(stream)
-    source.connect(processor)
-    source.connect(analyser)
-    processor.connect(context.destination)
+navigator.mediaDevices.getUserMedia({ audio: true, video: false, echoCancellation: true })
+.then(function (stream) {
+  const source = context.createMediaStreamSource(stream)
+  source.connect(processor)
+  source.connect(analyser)
+  processor.connect(context.destination)
 
-    visualisers[vIndex].init(analyser)
-    processor.onaudioprocess = function(e) {
-      visualisers[vIndex].process(analyser)
-      e.outputBuffer.getChannelData(0).forEach((v) => v = 0)
-    }
-  })
-  .catch((err) => window.alert(err))
-
-  canvas.style.display = 'block';
-  button.style.display = 'none';
-  initialised = true;
-}
+  visualisers[vIndex].init(analyser)
+  processor.onaudioprocess = function(e) {
+    visualisers[vIndex].process(analyser)
+    e.outputBuffer.getChannelData(0).forEach((v) => v = 0)
+  }
+})
+.catch(err => window.alert(err))
 
 const next = () => {
   vIndex = (vIndex + 1) % visualisers.length
   visualisers[vIndex].init(analyser)
 }
 
-document.body.onclick = () => {
-  initialised ? next() : start()
-}
+document.body.onclick = e => next()
--- src/visualisers/spectrum/index.js
+++ src/visualisers/spectrum/index.js
@@ -4,27 +4,28 @@ let offset = 0
 
 export default {
 
-  init: (analyser) => {
-    canvas.width = analyser.frequencyBinCount
-    canvas.height = window.innerHeight
-    canvas.style.height = '100vh';
+  init: analyser => {
+    canvas.width = window.innerWidth
+    canvas.height = analyser.frequencyBinCount
+    canvas.style.height = '100vh'
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    offset = canvas.width
   },
 
-  process: (analyser) => {
-    const spectrum = new Uint8Array(analyser.frequencyBinCount)
+  process: analyser => {
+    let spectrum = new Uint8Array(analyser.frequencyBinCount)
     analyser.getByteFrequencyData(spectrum)
 
-    const slice = ctx.getImageData(0, offset, canvas.width, 1)
+    const slice = ctx.getImageData(offset, 0, 1, canvas.height)
     for (let i = 0; i < spectrum.length; i++) {
-      slice.data[4 * i + 0] = spectrum[i]  // R
-      slice.data[4 * i + 1] = spectrum[i]  // G
-      slice.data[4 * i + 2] = spectrum[i]  // B
+      slice.data[4 * i + 0] = spectrum[spectrum.length - i]  // R
+      slice.data[4 * i + 1] = spectrum[spectrum.length - i]  // G
+      slice.data[4 * i + 2] = spectrum[spectrum.length - i]  // B
       slice.data[4 * i + 3] = 255          // A
     }
-    ctx.putImageData(slice, 0, offset)
+    ctx.putImageData(slice, offset, 0)
     offset += 1
-    offset %= canvas.height
+    offset %= canvas.width
   }
 
 }
